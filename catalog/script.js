2a0e94a3b29f104a14f2b4baadb33c8cbbb7be04
Dante_Alighiery <pavlokozban@gmail.com>
2021-06-05 00:13:40 +0300

    initCommit

---
+++ catalog/script.js
@@ -0,0 +1,92 @@
+"use strict"
+
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iOS/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows());
+        }
+     };
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+
+    let menuArrows = document.querySelectorAll('.menu__arrow');
+    // Перевірка овжини масиву
+    
+    if (menuArrows.length > 0) {
+        // цикл що б перевірити усі стрілки
+
+        for (let index = 0; index < menuArrows.length; index++) {
+            const menuArrow = menuArrows[index];
+
+            // Дія з кожною стрілкою (елементом) який є у масиві
+            menuArrow.addEventListener('click', function (e) {
+                // Навішування класу батьку елементу (Якщо його немає то забрати .toggle)
+                menuArrow.parentElement.classList.toggle('_active');
+            });
+        }
+    }
+
+} else {
+    document.body.classList.add('_pc');
+}
+
+
+
+function toggleMenu() {   // Подія клік на іконку, звернення до іконки добавка класу _active
+    const iconMenu = document.querySelector('.menu__icon'); // Звернення до іконки бургер
+    const menuBody = document.querySelector('.menu__body'); // Перевірка наяності класу menu__ion
+    document.body.classList.toggle('_lock'); // Заборона прогортання сторінки привідкритому меню бургер
+    iconMenu.classList.toggle('_active'); // Добавка класу 
+    menuBody.classList.toggle('_active');
+}
+
+
+$(function() {
+  $('.marquee').marquee({
+    duration: 7000,
+    startVisible: true,
+    duplicated: true,
+    gap:100
+  });
+});
+
+let btn = document.getElementById('btn');
+let btnText = document.getElementById('btnText');
+
+btn.onclick = function() {
+    btnText.innerHTML = "Дякуємо!";
+    btn.classList.add("active");
+}
+
+
+const nav = document.querySelector('#navbar');
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+    if(window.scroll >= topOfNav) {
+        document.sub-menu-container.classList.add('fixed-nav');
+    } else {
+        document.sub-menu-container.classList.remove('fixed-nav');
+    }
+
+}
+window.addEventListener('scroll' , fixNav);
