cb394dfa167ec4d451c694ad1d9ad831a224a35e
Dante_Alighiery <pavlokozban@gmail.com>
2021-06-22 22:04:59 +0300

    last

--- catalog/script.js
+++ catalog/script.js
@@ -95,3 +95,28 @@ $( window ).scroll(function() {
         $("#navbar").removeClass("sticky");
     }
 });
+
+
+$(function() {
+ 
+    $(window).scroll(function() {
+     
+    if($(this).scrollTop() != 0) {
+     
+    $('#toTop').fadeIn();
+     
+    } else {
+     
+    $('#toTop').fadeOut();
+     
+    }
+     
+    });
+     
+    $('#toTop').click(function() {
+     
+    $('body,html').animate({scrollTop:0},800);
+     
+    });
+     
+    });
